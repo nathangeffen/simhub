d2ef11e6f3dad7d959ce231ab1dd4d6773114fcf
Nathan Geffen <nathangeffen@gmail.com>
2020-01-03 16:12:12 +0200

    Changed cv.

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('list/', views.ArticleListView.as_view(), name='list'),
     path('article/<slug>/', views.article_view, name='detail'),
     path('about/', views.AboutView.as_view(), name='about'),
-    path('cv/', views.CvView.as_view(), name='cv')
+    path('nathan/', views.CvView.as_view(), name='cv')
 ]
